9bdb34360e5346949b9f500930ceeb581aa0c37e
marce <marcelofabiansalinas@gmail.com>
2023-04-04 16:28:44 -0300

    primer-guardado

---
+++ stock.js
@@ -0,0 +1,28 @@
+const productos = [
+    {
+        id : 1,
+        nombre: "Pulsar X2 Mini Wireless",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para claw grip y fingertip grip.",
+        precio: 95,
+        cantidad: 0
+    },
+    
+    {
+        id: 2,
+        nombre: "Vaxee Xe Orange",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 70,
+        cantidad: 0
+    },
+
+    {
+        id: 3,
+        nombre: "Logitech G Pro X Superlight",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 148,
+        cantidad: 0
+    }
+]
