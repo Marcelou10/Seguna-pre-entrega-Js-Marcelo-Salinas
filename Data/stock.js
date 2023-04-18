7228802d14a1d3055925cb0c6088bb5c30d27c49
marce <marcelofabiansalinas@gmail.com>
2023-04-18 17:59:41 -0300

    tercer commit

--- Data/stock.js
+++ Data/stock.js
@@ -1,28 +1,76 @@
 const productos = [
     {
         id : 1,
-        nombre: "Pulsar X2 Mini Wireless",
+        nombre: "Logitech G Pro X Superlight",
         tipo: "Mouse",
-        descripcion: "Mouse ambidiestro, hecho para claw grip y fingertip grip.",
-        precio: 95,
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 148,
         cantidad: 0
     },
     
     {
         id: 2,
+        nombre: "Logitech G303 Shroud Edition",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para claw y fingertip grip",
+        precio: 68,
+        cantidad: 0
+    },
+
+    {
+        id: 3,
+        nombre: "Logitech G502",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 39,
+        cantidad: 0
+    },
+    {
+        id: 4,
+        nombre: "Pulsar X2 Mini Wireless",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 95,
+        cantidad: 0
+    },
+    {
+        id: 5,
+        nombre: "Pulsar Xlite V2 Mini Blue Wireless",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 80,
+        cantidad: 0
+    },
+    {
+        id: 6,
+        nombre: "Pulsar Xlite White",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 50,
+        cantidad: 0
+    },
+    {
+        id: 7,
         nombre: "Vaxee Xe Orange",
         tipo: "Mouse",
         descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
         precio: 70,
         cantidad: 0
     },
-
     {
-        id: 3,
-        nombre: "Logitech G Pro X Superlight",
+        id: 8,
+        nombre: "Vaxee Zygen-NP01 Yellow",
         tipo: "Mouse",
         descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
-        precio: 148,
+        precio: 65,
+        cantidad: 0
+    },
+    {
+        id: 9,
+        nombre: "Vaxee OUTSET AX",
+        tipo: "Mouse",
+        descripcion: "Mouse ambidiestro, hecho para los tres tipos de agarre.",
+        precio: 65,
         cantidad: 0
     }
 ]
